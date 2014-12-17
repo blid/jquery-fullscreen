679655307ec1f0e83e9866a92f5c4c19e05b4f88
krzysiek <krzysiek@kucharski.net.pl>
2014-12-18 00:05:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# jQuery fullscreen plugin for all browsers
+
+Allows to display given element or whole `document` in fullscreen mode by using [requestFullscreen API](https://developer.mozilla.org/en-US/docs/Web/API/Element.requestFullScreen).
+
+Gracefully degrades to fake fullscreen (div covering whole viewport) when requestFullscreen is not supported at all. Plugin is written to make use all of the vendor implementations (currently all major browsers use vendor prefixes).
+
+## Usage
+
+In order to work plugin must be called on **real click or keyboard event**.
+
+### Simple example
+
+```javascript
+ $('a').click(function() {
+   $('div').fullscreen().request();
+ }
+```
+
+Code above will open given element in Fullscreen mode. Simple, right?
+Check `demo` directory to see some working implementation.
+
+### Available methods
+
+* `request()` open given element or document in Fullscreen mode
+* `cancel()` close
+* `isActive()` return `true` or `false` basing on whether full page is in fullscreen mode
+* `toggle()` toggle between fullscreen mode on and off (useful when you want just one button)
+
+### Events
+
+When using any of switching methods (`request()`, `cancel()` or `toggle()`) `fullScreenChange` event is sent on `document` element.
+
+Example:
+
+```javascript
+ $(document).on('fullScreenChange', function() {
+   console.log('Full screen toggled!'); 
+ });
+```
+
+## Browser support
+
+Since plugin will gracefully degrade for users without [requestFullscreen API](https://developer.mozilla.org/en-US/docs/Web/API/Element.requestFullScreen) it's not a big deal.
+
+According to [caniuse.com usage statistics](http://caniuse.com/#feat=fullscreen) native Fullscreen API is supported in **67%** of global browsers.
+
+## Are there any tests ?
+
+Unfortunately no, mostly because of browser limitation (no fake clicks). However, if you have an idea how to overcome this in simple manner please let me know.
+
+## License
+
+MIT
+
+## Contributing 
+
+Fork, make your feature/bugfix branch and create pull request. 
+
+## Development plans
+
+* Make plugin UMD compatible
+* Prepare bower package
+* Prepare minimized, distribution ready version using gulp workflow
+
+
+
+
+
+
---
+++ jquery.fullscreen.js
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2014 Krzysztof Kucharski
+ */
+
+(function ($) {
+    'use strict';
+    /*
+     * Element which is fullscren called on
+     */
+    var element = document.documentElement;
+    /*
+     * Currently used adapter
+     */
+    var adapter = null;
+
+    var doc = document.documentElement;
+    var _adapters = [NativeAdapter, WebkitAdapter, MozillaAdapter, MicrosoftAdapter, NotSupportedAdapter];
+
+    /*
+     * Bind browser specific event and trigger jQuery event
+     */
+    function _bindAndTriggerEvents(adapter) {
+        $(document).off(adapter.eventName).on(adapter.eventName, function () {
+            if (adapter.isActive()) {
+                $(document).trigger(new jQuery.Event("enterFullScreen"));
+            } else {
+                $(document).trigger(new jQuery.Event("exitFullScreen"));
+            }
+
+            $(document).trigger(new jQuery.Event("fullScreenChange"), {isActive: adapter.isActive()});
+        });
+    }
+
+    function NativeAdapter() {
+    }
+
+    function WebkitAdapter() {
+    }
+
+    function MozillaAdapter() {
+    }
+
+    function NotSupportedAdapter() {
+    }
+
+    function MicrosoftAdapter() {
+
+    }
+
+    $.extend(NativeAdapter.prototype, {
+        isAvailabile: function () {
+            return !!doc.requestFullscreen;
+        },
+
+        request: function () {
+            element.requestFullscreen();
+        },
+
+        cancel: function () {
+            document.exitFullscreen();
+        },
+
+        isActive: function () {
+            return !!document.fullscreenElement;
+        },
+
+        eventName: "fullscreenchange"
+    });
+
+    $.extend(WebkitAdapter.prototype, {
+        isAvailabile: function () {
+            return !!doc.webkitRequestFullscreen;
+        },
+
+        request: function () {
+            element.webkitRequestFullscreen();
+        },
+
+        cancel: function () {
+            document.webkitCancelFullScreen();
+        },
+
+        isActive: function () {
+            return document.webkitIsFullScreen;
+        },
+
+        eventName: "webkitfullscreenchange"
+    });
+
+    $.extend(MozillaAdapter.prototype, {
+        isAvailabile: function () {
+            return !!doc.mozRequestFullScreen;
+        },
+
+        request: function () {
+            element.mozRequestFullScreen();
+        },
+
+        cancel: function () {
+            document.mozCancelFullScreen();
+        },
+
+        isActive: function () {
+            return window.fullScreen;
+        },
+
+        eventName: "mozfullscreenchange"
+    });
+
+    $.extend(MicrosoftAdapter.prototype, {
+        isAvailabile: function () {
+            return !!doc.msRequestFullscreen;
+        },
+
+        request: function () {
+            element.msRequestFullscreen();
+        },
+
+        cancel: function () {
+            document.msExitFullscreen();
+        },
+
+        isActive: function () {
+            return !!document.msFullscreenElement;
+        },
+
+        eventName: "MSFullscreenChange"
+    });
+
+
+    // Not support browsers like IE8-10, just made the container pop above everything else
+    $.extend(NotSupportedAdapter.prototype, {
+        isAvailabile: function () {
+            return true;
+        },
+        request: function () {
+            var self = this;
+            window.fakeFullScreenActive = true;
+
+            var $window = $(window),
+                $element = $(element),
+                viewport_dimensions = {width: $window.width(), height: $window.height()},
+                margin = 0;
+
+            $element.css({
+                width: viewport_dimensions.width - margin * 2,
+                height: viewport_dimensions.height - margin * 2
+            }).addClass('fakeFullScreen');
+
+            $j(document).on('keyup.cancelFakeFullScreen', function (e) {
+                if (e.keyCode == 27) {
+                    self.cancel();
+                }
+            });
+
+            $(document).trigger(this.eventName);
+        },
+        cancel: function () {
+            window.fakeFullScreenActive = false;
+
+            var $element = $(element);
+            $element.removeClass('fakeFullScreen').css({
+                width: '100%',
+                height: '100%'
+            });
+
+            $(document).off('keyup.cancelFakeFullScreen');
+            $(document).trigger(this.eventName);
+        },
+
+        isActive: function () {
+            return window.fakeFullScreenActive;
+        },
+
+        eventName: "notsupportfullscreenchange"
+    });
+
+    var toggleExtension = {
+        toggle: function () {
+            if (!this.isActive()) {
+                this.request();
+            } else {
+                this.cancel();
+            }
+        }
+    };
+
+    $.fn.fullscreen = function () {
+        if (this.length > 1) {
+            throw new Error("This plugin can be called on one element at once.");
+        }
+        if (!$.data(this[0], 'fullscreen')) {
+            $.each(_adapters, function (i, val) {
+                var implementation = new val();
+                if (implementation.isAvailabile()) {
+                    adapter = implementation;
+                    _bindAndTriggerEvents(adapter);
+                    $.extend(adapter, toggleExtension);
+                    // way to check outside whether browser supports fullscreen API
+                    $.support.fullscreen = true;
+                    return false;
+                }
+            });
+            element = this[0];
+
+            $.data(this[0], 'fullscreen', adapter);
+        }
+        return $.data(this[0], 'fullscreen');
+    }
+})(jQuery);
